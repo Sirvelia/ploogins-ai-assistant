a89fb76d1b460caaaaf695181cbf6c44a69d1712
Amadeu Arderiu <amadeu@sirvelia.com>
2025-07-03 08:47:36 +0200

    fix deployment

--- ploogins-ai-assistant.php
+++ ploogins-ai-assistant.php
@@ -5,7 +5,7 @@
  * Plugin Name:       Ploogins - AI Assistant
  * Plugin URI:        https://ploogins.com/
  * Description:       AI Assistant
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Ploogins
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
